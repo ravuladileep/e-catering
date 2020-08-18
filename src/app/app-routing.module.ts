ae49c06b9f39685db22146babc08381d1f32cc39
dileep <ravuladileep98@gmail.com>
2020-08-18 17:42:31 +0530

    databinding and filtering

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { OrdersComponent } from './components/orders/orders.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 const routes: Routes = [
-  {path: '', component: DashboardComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: 'orders', component: OrdersComponent}
 
 ];
--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -6,10 +6,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe.component.scss']
 })
 export class RecipeComponent implements OnInit {
+  public searchTerm;
+  public items = [
+    {
+      subsection: [
+        {
+        name: 'GRILLED MARINATED FLANK STEAK: GF, DF, NF',
+        description: 'With Smoked Tomato BBO Glaze (Serves 4)',
+        price: '$45.00'
+        },
+        {
+        name: 'LEMON DIJON CRUSTED SALMON: GF, DF',
+        description: 'With Blistered Tomatoes (Serves 4)',
+        price: '$45.00'
+        },
+        {
+        name: 'Red and White Tortellini.: GF, DF, NF',
+        description: 'With Smoked Tomato BBO Glaze (Serves 4)',
+        price: '$45.00'
+        },
+        {
+        name: 'Classic Meatloaf: GF, DF, NF',
+        description: 'With Smoked Tomato BBO Glaze (Serves 4)',
+        price: '$45.00'
+        }]
+    },
+    {
+      subsection: [
+        {
+        name: 'Baked Honey Mustard Chicken: GF, DF, NF',
+        description: 'With Smoked Tomato BBO Glaze (Serves 4)',
+        price: '$45.00'
+        },
+        {
+        name: 'LEMON DIJON CRUSTED SALMON: GF, DF',
+        description: 'With Blistered Tomatoes (Serves 4)',
+        price: '$45.00'
+        },
+        {
+        name: 'Meatball Stew Casserole: GF, DF, NF',
+        description: 'With Smoked Tomato BBO Glaze (Serves 4)',
+        price: '$45.00'
+        },
+        {
+        name: 'GRILLED MARINATED FLANK STEAK: GF, DF, NF',
+        description: 'With Smoked Tomato BBO Glaze (Serves 4)',
+        price: '$45.00'
+        }]
+    }
 
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
