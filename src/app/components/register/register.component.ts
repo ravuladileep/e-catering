fc2251bebdf971edc9d154259ccac4f22f652075
dileep <ravuladileep98@gmail.com>
2020-08-26 09:32:22 +0530

    added reactive form for registration

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  public register: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm();
+  }
+
+  registerForm(): void {
+    this.register = this.fb.group({
+      firstName : ['', [Validators.required]],
+      lastName : ['', [Validators.required]],
+      userName : ['', [Validators.required]],
+      password : ['', [Validators.required]],
+      confirmPassword : ['', [Validators.required]],
+      email : ['', [Validators.required]],
+      phone : ['', [Validators.required]],
+      address : ['', [Validators.required]],
+    });
+  }
+
+  submit(): void{
+    console.log(this.register.value)
   }
 
 }
