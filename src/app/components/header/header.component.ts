672c4f757988e2062357b8e2955608bc1d54f6da
dileep <ravuladileep98@gmail.com>
2020-09-14 17:02:54 +0530

    added logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, DoCheck {
+  public loginres = false;
+  public loginresponseData;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  ngDoCheck(){
+    if(JSON.parse(sessionStorage.getItem('loginResponse'))){
+      this.loginres = true;
+      this.loginresponseData = JSON.parse(sessionStorage.getItem('loginResponse'));
+    }else {
+      this.loginres = false;
+    }
+  }
+
+  public logOut(){
+    sessionStorage.removeItem('loginResponse');
+    this.router.navigate(['home']);
   }
 
 }
