254017f3d1e3a73bf535a5baabac850cda30ef94
dileep <ravuladileep98@gmail.com>
2020-09-28 10:54:51 +0530

    added scroll on click

--- src/app/components/cater-theme-one-header/cater-theme-one-header.component.ts
+++ src/app/components/cater-theme-one-header/cater-theme-one-header.component.ts
@@ -1,9 +1,9 @@
-import { ElementRef, Input, Output, ViewChild } from '@angular/core';
+import { DoCheck, ElementRef, Input, Output, ViewChild } from '@angular/core';
 import { Component, EventEmitter, OnInit } from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { CartService } from 'src/app/services/cart.service';
-
+declare var $:any;
 @UntilDestroy()
 @Component({
   selector: 'app-cater-theme-one-header',
@@ -11,7 +11,7 @@ import { CartService } from 'src/app/services/cart.service';
   styleUrls: ['./cater-theme-one-header.component.scss'],
 })
 export class CaterThemeOneHeaderComponent implements OnInit {
-  // @ViewChild('widgetsContent', { read: ElementRef }) public widgetsContent: ElementRef<any>;
+  @ViewChild('widgetsContent', { read: ElementRef }) public widgetsContent: ElementRef<any>;
   public newMenu = [];
   public category = [];
   public subCategory = [];
@@ -21,16 +21,44 @@ export class CaterThemeOneHeaderComponent implements OnInit {
 
   ngOnInit(): void {
     this.getItems();
+    // this.dropdownMenu();
   }
 
-
-  // public scrollRight(): void {
-  //   this.widgetsContent.nativeElement.scrollTo({ left: (this.widgetsContent.nativeElement.scrollLeft + 150), behavior: 'smooth' });
+  // ngDoCheck(){
+  //   this.dropdownMenu();
   // }
 
-  // public scrollLeft(): void {
-  //   this.widgetsContent.nativeElement.scrollTo({ left: (this.widgetsContent.nativeElement.scrollLeft - 150), behavior: 'smooth' });
+  // public dropdownMenu(){
+  //     $('.dropdown').on('mouseenter', function() {
+  //       var $btnDropDown = $(this).find('.dropdown-toggle');
+  //       var $listHolder = $(this).find('.dropdown-menu');
+  //       // reset position property for DD container
+  //       $(this).css('position', 'static');
+  //       $listHolder.css({
+  //         // "top": ($btnDropDown.offset().top + $btnDropDown.outerHeight(true)) + "px",
+  //         'left': $btnDropDown.offset().left + 'px'
+  //       });
+  //       $listHolder.data('open', true);
+  //     });
+  //     // add BT DD hide event
+  //     $('.dropdown').on('mouseleave', function() {
+  //       var $listHolder = $(this).find('.dropdown-menu');
+  //       $(this).css('position', '');
+  //       $listHolder.css({
+  //         "top": '',
+  //         'left': ''
+  //       });
+  //       $listHolder.data('open', false);
+  //     });
   // }
+
+  public scrollRight(): void {
+    this.widgetsContent.nativeElement.scrollTo({ left: (this.widgetsContent.nativeElement.scrollLeft + 150), behavior: 'smooth' });
+  }
+
+  public scrollLeft(): void {
+    this.widgetsContent.nativeElement.scrollTo({ left: (this.widgetsContent.nativeElement.scrollLeft - 150), behavior: 'smooth' });
+  }
 
   public getItems(): void {
     this.spinner.show();
